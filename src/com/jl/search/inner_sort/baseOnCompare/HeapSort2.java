0ec21f6d50d05a4511515ab55e9740f8f5c7d908
apprentice121jl <ju_longg@163.com>
2020-04-08 10:49:15 +0800

    commit:heap sort

--- src/com/jl/search/inner_sort/baseOnCompare/HeapSort2.java
+++ src/com/jl/search/inner_sort/baseOnCompare/HeapSort2.java
@@ -5,43 +5,54 @@ import java.util.Arrays;
 /**
  * JL
  * 2020/4/7  22:05
+ * 不稳定排序
  **/
-public class HeapSort2 {
+public class HeapSort2  {
 
     public static void main(String[] args) {
 
         Integer[] arr = {5,0,-1,11,20,2,-2,4,-6,3,-7,-7,8,1};
         int length = arr.length;
         int i;
-        // TODO i为何要从length/2开始？
-        for(i = length/2;i >= 0;i--){// 从二叉树第一个非叶子结点建立堆
+        for( i = (length/2)-1;i >= 0;i-- ){// 从二叉树第一个非叶子结点建立堆
+            // 树的底部开始建树，根节点是最大值
             shift(arr,i,length);
         }
 
         System.out.println(Arrays.toString(arr));
-        for(int j = length-1;j >= 1;j--){// 只需要判断n-1个结点有序
+
+
+        int  j = length-1;
+        while(j >= 1){
+
             int tmp=arr[0];
             arr[0] = arr[j];
             arr[j] = tmp;
-            shift(arr,0,j-1);
+
+            // 从根节点开始调整，由于建堆的时候已经有序，所以可以从根节点开始
+            shift(arr,0,--j);
+
         }
         System.out.println(Arrays.toString(arr));
     }
 
-    static void shift(Integer[] arr,int i,int n){// 每次获得的根都是最大值
-        int temp = arr[i];
-        int parent = i;
-        int child = 2*i;
+    //
+    static void shift(Integer[] arr,int i,int length){// 每次获得的根都是最大值
 
-        while(child < n){// 结点在有效范围内
+        int temp = arr[i];
+
+        int parent = i;
+        int child = 2*i+1;
+
+        while(child < length){// 结点在有效范围内
             // 判断右孩子是否存在且大于左孩子
-            if(child+1 < n && arr[child] < arr[child + 1]){
+            if(child+1 < length && arr[child] < arr[child + 1]){
                  child++;
             }
             if(temp < arr[child]){// 父节点小于孩子结点
                 arr[parent]=arr[child];
                 parent = child;
-                child = 2*parent;
+                child = 2*parent+1;
             }else{
                 break;
             }
